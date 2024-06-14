bd3e54c35057aa3741cbd407cad14a9c5d77427d
Iryna <luspus11@gmail.com>
2024-06-14 13:19:12 +0300

    Requirement 2

--- src/calc.js
+++ src/calc.js
@@ -5,22 +5,30 @@ function stringCalculator(numbers):number {
   return -1;
 }
 
-function resultAsMessage(testCaseName, expected, actual): string {
-  console.log(expected, actual)
-  return expected === actual
+function resultAsMessage(testCaseName, number, actual): string {
+  const expected: string  =  number === '' ? 'empty' : number;
+  return number === actual
     ? `${testCaseName}: PASS`
     : `${testCaseName}: FAIL (expected ${expected}, actual ${actual})`;
 }
 
-function main(number, expected):number {
-  console.log("Running tests...");
-  
+function main1(number, expected):number {
+  console.log("Running tests 1...");
+
   // Requirement 1
   let result: number = stringCalculator(number);
   resultAsMessage("Requirement 1", expected, result);
   return result;
 }
 
+function main2 (number): number {
+  console.log("Running tests 2...");
+  // Requirement 2
+  let result: number = stringCalculator(number);
+  console.log(resultAsMessage(`Requirement 2 (${number})`, number, result));
+  return result;
+}
 
 
-module.exports = main;
+module.exports = main1;
+module.exports = main2;
--- src/calc.test.js
+++ src/calc.test.js
@@ -1,6 +1,14 @@
-const main = require('./calc');
-test('If the input string is empty, the result should be 0', () => {
-  expect(main('', 0)).toBe(0);
+const main1 = require('./calc');
+const main2 = require('./calc');
+
+// Requirement 1
+test('If the input string is empty, the result should be 0', (): void => {
+  expect(main1('', 0)).toBe(0);
 });
 
-
+// Requirement 2
+test('If string has a single number, the result should be the value of that number.', (): void => {
+  expect(main2('')).toBe(0);
+  expect(main2(321)).toBe(-1);
+  expect(main2(1)).toBe(-1);
+});
